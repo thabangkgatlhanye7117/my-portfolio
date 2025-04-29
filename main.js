42a74a0b07dc14ccccf8f331b458f08916178620
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-04-29 17:48:51 +0200

    project finalization

--- main.js
+++ main.js
@@ -33,9 +33,91 @@ const linkAction = () =>{
 }
 
 navLink.forEach(l => l.addEventListener('click' , linkAction))
+/*shadow header */
 
+const shadowHeader = () => {
 
+      const header = document.getElementById('header');
 
+      this.scrollY >= 50 ? header.classList.add('shadow-header')
+                           :header.classList.remove('shadow-header')
+    
+}
+window.addEventListener('scroll', shadowHeader);
+
+/*Email*/
+const contactForm = document.getElementById('contact-form')
+
+/*SHOW SCROLL UP*/
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+} 
+window.addEventListener('scroll', scrollUp);
+
+/*Scroll sections active link */
+
+const sections = document.querySelectorAll('section[id]');
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 58,
+          sectionId = current.getAttribute('id'),
+          sectionsClass = document.querySelector(".nav-menu a[href*=" + sectionId + ']');
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+
+}
+window.addEventListener('scroll', scrollActive)
+
+/*Dark Light Theme */
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+//Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+//We validate if the user previously chose a topic
+
+if(selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'] (darkTheme);
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'] (iconTheme)
+}
+
+//Activate/Deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  //Add or Remove the dark/icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  //We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+//Scroll reveal animation
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true // Animations repeat
+})
+sr.reveal(`.home-profile`, {origin:'right'})
+sr.reveal(`.home-name, .home-info`, {origin:'left'})
 
 
 
